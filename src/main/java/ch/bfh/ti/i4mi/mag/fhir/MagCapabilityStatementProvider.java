e2e834ad043f2a0f7d2c1c81a1905f60daf8b96f
Luis Filipe de Sousa <lde.sousa@gmail.com>
2024-08-12 14:17:54 +0200

    feat: Added the capability statements that confirms tha pagination is off

--- src/main/java/ch/bfh/ti/i4mi/mag/fhir/MagCapabilityStatementProvider.java
+++ src/main/java/ch/bfh/ti/i4mi/mag/fhir/MagCapabilityStatementProvider.java
@@ -9,19 +9,24 @@ import org.hl7.fhir.instance.model.api.IBaseConformance;
 import org.hl7.fhir.r4.model.CapabilityStatement;
 import org.hl7.fhir.r4.model.Extension;
 import org.hl7.fhir.r4.model.StringType;
+import org.hl7.fhir.r4.model.BooleanType;
 import org.openehealth.ipf.commons.ihe.fhir.support.NullsafeServerCapabilityStatementProvider;
 import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * A customized provider of the server CapabilityStatement that adds the OAuth URIs extension.
+ * A customized provider of the server CapabilityStatement that adds the OAuth URIs extension
+ * and no-paging configuration.
  *
  * @author Quentin Ligier
+ * @author Luis Filipe de Sousa
  */
 @Interceptor
 public class MagCapabilityStatementProvider extends NullsafeServerCapabilityStatementProvider {
-
+    private static final Logger logger = LoggerFactory.getLogger(MagCapabilityStatementProvider.class);
     private final String baseUrl;
 
     public MagCapabilityStatementProvider(final RestfulServer fhirServer,
@@ -33,14 +38,28 @@ public class MagCapabilityStatementProvider extends NullsafeServerCapabilityStat
 
     @Override
     public IBaseConformance getServerConformance(HttpServletRequest theRequest, RequestDetails theRequestDetails) {
+        logger.info("Generating CapabilityStatement with OAuth URIs and no-paging configuration");
         final var conformance = (CapabilityStatement) super.getServerConformance(theRequest, theRequestDetails);
 
+        // Add OAuth URIs extension
         conformance.getRestFirstRep().getSecurity().addExtension()
                 .setUrl("http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris")
                 .addExtension(new Extension("token",
-                                            new StringType(this.baseUrl + TokenEndpointRouteBuilder.TOKEN_PATH)))
+                        new StringType(this.baseUrl + TokenEndpointRouteBuilder.TOKEN_PATH)))
                 .addExtension(new Extension("authorize",
-                                            new StringType(this.baseUrl + Iti71RouteBuilder.AUTHORIZE_PATH)));
+                        new StringType(this.baseUrl + Iti71RouteBuilder.AUTHORIZE_PATH)));
+
+        // Add no-paging configuration
+        // CapabilityStatement.CapabilityStatementRestComponent rest = conformance.getRestFirstRep();
+        // Extension noPagingExtension = new Extension("http://example.com/fhir/StructureDefinition/no-paging");
+        // noPagingExtension.setValue(new BooleanType(true));
+        // rest.addExtension(noPagingExtension);
+
+        // Remove any existing paging-related search parameters
+        // rest.getResource().forEach(resource ->
+        //        resource.getSearchParam().removeIf(param -> param.getName().equals("_count")));
+
+        logger.debug("Updated CapabilityStatement: {}", conformance);
 
         return conformance;
     }
